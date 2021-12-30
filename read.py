0b218810a6f82da3dd4a2f6acad4ed88f6ef992e
kaoger <rend579@gmail.com>
2021-12-30 15:28:16 +0800

    新增篩選

--- read.py
+++ read.py
@@ -7,3 +7,14 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data),'筆資料')
+
+sunlen = 0
+for d in data:
+	sunlen += len(d)
+print('平均是', sunlen/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new), '筆留言字數小於100')
